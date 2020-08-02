abf4dd5bf66f2912ffdedb209139d2769a924875
AMAL MOHAN K <amalmohan542@gmail.com>
2020-08-03 00:48:23 +0530

    Image converted To black and white And Displayed on a Window-Day-1 Works End

--- AIDetectionCarAndPedestrians.py
+++ AIDetectionCarAndPedestrians.py
@@ -9,8 +9,26 @@ classifier_file="car_detector.xml"
 #Create OpenCV image
 img=cv2.imread(img_file)
 
+#Convert to black and white image inorder to use Haar Features
+# and makes the Algo Fast Without Dealing With colour Data.
+blackandwhite= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#Create a car Classifier
+car_tracker=cv2.CascadeClassifier(classifier_file)
+
+
+
+
+
+
+
+
+
+
+
+
 #Display the images with Cars Spotted
-cv2.imshow("AI car Detector", img)
+cv2.imshow("AI car Detector", blackandwhite)
 
 #Dont Auto close window and waite for a key presse
 cv2.waitKey()
