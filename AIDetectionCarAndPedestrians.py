88787595d4a45f40b812bba59946660d35b4e4ee
AMAL MOHAN K <amalmohan542@gmail.com>
2020-08-03 14:01:03 +0530

    Final Commit of AI Based Car and pedestrians tracking for self driving car Project

--- AIDetectionCarAndPedestrians.py
+++ AIDetectionCarAndPedestrians.py
@@ -55,40 +55,4 @@ while True:
 #Release Video Capture File Object
 Video.release()
 
-
-
-"""
-
-#Create OpenCV image
-img=cv2.imread(img_file)
-
-#Convert to black and white image inorder to use Haar Features
-# and makes the Algo Fast Without Dealing With colour Data.
-blackandwhite= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-#Create a car Classifier
-car_tracker=cv2.CascadeClassifier(classifier_file)
-
-#Car Tracker (Detect Cars of Multile Size or Scale)
-cars=car_tracker.detectMultiScale(blackandwhite)
-
-#Draw Rectangles around Cars Detected
-car1=cars[3]
-for (x,y,w,h) in cars:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-
-
-
-
-
-#Display the images with Cars Spotted
-cv2.imshow("AI car Detector", img)
-
-
-#Dont Auto close window and waite for a key presse
-cv2.waitKey()
-
-"""
-
-
 print("Code Completed Sucessfully")
