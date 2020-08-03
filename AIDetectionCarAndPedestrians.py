c6cf0a7b1f63afe9aac3ca76b76cdce01257522c
AMAL MOHAN K <amalmohan542@gmail.com>
2020-08-03 11:43:56 +0530

    Detected the one car on image git add .

--- AIDetectionCarAndPedestrians.py
+++ AIDetectionCarAndPedestrians.py
@@ -16,19 +16,21 @@ blackandwhite= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #Create a car Classifier
 car_tracker=cv2.CascadeClassifier(classifier_file)
 
+#Car Tracker (Detect Cars of Multile Size or Scale)
+cars=car_tracker.detectMultiScale(blackandwhite)
 
-
-
-
-
-
+#Draw Rectangles around Cars Detected
+car1=cars[0]
+(x,y,w,h)=car1
+cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
 
 
 
 
 
 #Display the images with Cars Spotted
-cv2.imshow("AI car Detector", blackandwhite)
+cv2.imshow("AI car Detector", img)
+
 
 #Dont Auto close window and waite for a key presse
 cv2.waitKey()
